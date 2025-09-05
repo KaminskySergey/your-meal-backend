722300f1c5a667f031511d311eaf195b0ac73c5e
Serhii Kaminskyi <kaminsky_s_s@ukr.net>
2025-09-05 15:28:53 +0300

    add n1

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: [
-      'http://localhost:5174', 
-      'https://your-meal-backend.vercel.app/' 
-    ],
-    credentials: true, 
-  });
+  app.enableCors({ origin: '*', credentials: true });
 
   app.setGlobalPrefix('api');
 
